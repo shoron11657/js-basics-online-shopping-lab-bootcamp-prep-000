546cbc8502d0b4ea21e1dc1f8d5eb46815eb2172
Kevin M <shoron@gmail.com>
2017-11-18 05:39:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ function removeFromCart(item) {
       console.log(cart)}
     else {console.log('That item is not in your cart.')
       }}}
-return cart      
+return cart
 }
 
 function placeOrder(cardNumber) {
