772c934d1fc9e803bdced95887874d7586e772b9
Kevin M <shoron@gmail.com>
2017-11-18 06:25:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -50,14 +50,12 @@ function removeFromCart(item) {
   var present = false
   if (cart.length > 0){
   for (var i = 0; i < cart.length; i++){
-    debugger
     if (cart[i].hasOwnProperty(item) === true){
       cart.splice(i, 1)
       console.log(cart)
-      present = true}
-          }}
-if (present === false){console.log('That item is not in your cart.')}
-return cart
+      present = true}}}
+  if (present === false){console.log('That item is not in your cart.')}
+  return cart
 }
 
 function placeOrder(cardNumber) {
